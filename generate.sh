eec4a4425c27b24c6be11c2a50c68df76fbd4f98
Hideo Hattori <hattori-h@klab.com>
2016-12-13 01:55:18 +0900

    fix workspace path

--- generate.sh
+++ generate.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -ue
 
 JUBATUS_DIR="jubatus-generate"
-JUBATUS_BRANCH="master"
+#JUBATUS_BRANCH="master"
+JUBATUS_BRANCH="support-rust-client-for-jenerator"
 CLIENT_DIR="$(cd $(dirname ${0}) && echo ${PWD})"
 
 [ $# -eq 0 ] || JUBATUS_BRANCH="${1}"
 
 rm -rf "${JUBATUS_DIR}"
-#git clone https://github.com/jubatus/jubatus.git "${JUBATUS_DIR}"
-cp -r $HOME/jubarust/jubatus/ "${JUBATUS_DIR}"
-#pushd "${JUBATUS_DIR}"
-#git checkout "${JUBATUS_BRANCH}"
-#popd
+git clone https://github.com/hhatto/jubatus.git "${JUBATUS_DIR}"
+pushd "${JUBATUS_DIR}"
+git checkout "${JUBATUS_BRANCH}"
+popd
 
-# Go
+# Rust
 
 capitalize() {
 	echo "$(echo ${1:0:1} | tr 'a-z' 'A-Z')${1:1}"
