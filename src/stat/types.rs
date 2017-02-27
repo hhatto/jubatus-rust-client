326edfb223f71f3eba9038ffa386fad7fcb96216
Hideo Hattori <hattori-h@klab.com>
2017-02-27 19:56:59 +0900

    update

--- src/stat/types.rs
+++ src/stat/types.rs
@@ -1,6 +1,2 @@
 // This file is auto-generated from stat.idl(0.6.4-33-gcc8d7ca9) with jenerator version 1.0.0-27-ge6a9293f/support-rust-client-for-jenerator
 // *** DO NOT EDIT ***
-
-use std::collections::HashMap;
-use common::datum::Datum;
-use rmpv::Value;
